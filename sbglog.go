79bd0caf4e0dc6c58528aaa1d039df7fb1a3adca
voyager3m <voyager3m@gmail.com>
2017-08-25 22:55:19 +0300

    add other priority

--- sbglog.go
+++ sbglog.go
@@ -7,14 +7,20 @@ import (
 	"os"
 	"runtime"
 	"strings"
+	"sync"
+	"syscall"
 	"time"
 )
 
 var (
-	name_    string = os.Args[0]
-	addr_    string = "127.0.0.1:1514"
-	conn_    net.Conn
-	errtypes = []string{"EMERG", "ALERT", "CRITCL", "ERROR", "WARNG", "NOTE", "INFO", "DEBUG"}
+	name_        string = os.Args[0]
+	addr_        string = "127.0.0.1:1514"
+	conn_        net.Conn
+	errtypes          = []string{"EMERG", "ALERT", "CRITCL", "ERROR", "WARNG", "NOTE", "INFO", "DEBUG"}
+	usegorutine_ bool = false
+	consoleout_  bool = false
+	syslogout_   bool = false
+	wg_          sync.WaitGroup
 )
 
 func init() {
@@ -27,8 +33,24 @@ func init() {
 	}
 }
 
+func SetConsoleOut(out bool) {
+	consoleout_ = out
+}
+
+func SetSyslogOut(out bool) {
+	syslogout_ = out
+}
+
+func UseGorutine(use bool) {
+	usegorutine_ = use
+}
+
 func SetName(name string) {
 	name_ = name
+}
+
+func Wait() {
+	wg_.Wait()
 }
 
 func SetAddr(addr string) {
@@ -45,21 +67,112 @@ func SetAddr(addr string) {
 	}
 }
 
-func Err(i interface{}) {
+func Emergency(i interface{}) {
 	s := fmt.Sprint(i)
-	log(3, s)
+	var _, file, line, _ = runtime.Caller(1)
+	if usegorutine_ {
+		wg_.Add(1)
+		go log(0, file, line, s)
+	} else {
+		log(0, file, line, s)
+	}
 
 }
 
-func log(eventtype int, str string) {
+func Alert(i interface{}) {
+	s := fmt.Sprint(i)
 	var _, file, line, _ = runtime.Caller(1)
-	var pid = os.Getgid()
+	if usegorutine_ {
+		wg_.Add(1)
+		go log(1, file, line, s)
+	} else {
+		log(1, file, line, s)
+	}
+
+}
+
+func Critical(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	if usegorutine_ {
+		wg_.Add(1)
+		go log(2, file, line, s)
+	} else {
+		log(2, file, line, s)
+	}
+}
+
+func Error(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	if usegorutine_ {
+		wg_.Add(1)
+		go log(3, file, line, s)
+	} else {
+		log(3, file, line, s)
+	}
+}
+
+func Warning(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	if usegorutine_ {
+		wg_.Add(1)
+		go log(4, file, line, s)
+	} else {
+		log(4, file, line, s)
+	}
+}
+
+func Note(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(2)
+	if usegorutine_ {
+		wg_.Add(1)
+		go log(5, file, line, s)
+	} else {
+		log(5, file, line, s)
+	}
+}
+
+func Info(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(2)
+	if usegorutine_ {
+		wg_.Add(1)
+		go log(6, file, line, s)
+	} else {
+		log(6, file, line, s)
+	}
+}
+
+func Debug(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(2)
+	if usegorutine_ {
+		wg_.Add(1)
+		go log(7, file, line, s)
+	} else {
+		log(7, file, line, s)
+	}
+}
+
+func log(eventtype int, file string, line int, str string) {
+	var pid = syscall.Getpid() //os.Getgid()
 	var sf = strings.Split(file, "/")
 	file = sf[len(sf)-1]
 	t := time.Now().UTC()
-	strt := t.Format(time.ANSIC)
+	strt := t.Format("Jan 2 15:04:05 2006")
 
-	var fmtstr = fmt.Sprintf("<%d>%.15s %s[%d]: [%s:%d]: %s", eventtype, strt, name_, pid, file, line, str)
+	var fmtstr = fmt.Sprintf("<%d>%.15s %s[%d]: %s:%d %s: %s", eventtype, strt, name_, pid, file, line, errtypes[eventtype], str)
 	fmt.Fprintf(conn_, fmtstr)
+	if consoleout_ {
+		fmt.Fprintf(os.Stderr, "%s\n", fmtstr)
+	}
+	if syslogout_ {
 
+	}
+	if usegorutine_ {
+		wg_.Done()
+	}
 }
