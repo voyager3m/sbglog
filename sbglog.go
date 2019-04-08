634ef95cd89d825b08cc1b5dc906c88cc2439d4d
Alex <voyager3m@gmail.com>
2019-04-08 20:34:42 +0300

    add loglevel

--- sbglog.go
+++ sbglog.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"runtime"
 	"strings"
-	"sync"
 	"syscall"
 	"time"
 )
@@ -22,7 +21,8 @@ var (
 	consoleout_  bool = false
 	syslogout_   bool = false
 	syslog_      *syslog.Writer
-	wg_          sync.WaitGroup
+	loglevel_    int = 7
+	pid_         int
 )
 
 func init() {
@@ -34,6 +34,7 @@ func init() {
 		fmt.Fprintf(os.Stderr, "Error connect to sbg server %v", err)
 
 	}
+	pid_ = syscall.Getpid()
 }
 
 func UseConsole(out bool) {
@@ -57,14 +58,6 @@ func UseSyslog(addr string) {
 
 }
 
-func UseGorutine(use bool) {
-	usegorutine_ = use
-}
-
-func Wait() {
-	wg_.Wait()
-}
-
 func SetName(name string) {
 	name_ = name
 }
@@ -84,119 +77,124 @@ func SetAddr(addr string) {
 	}
 }
 
+/*set max loglevel , from 0 (nothing) to 7(debug) */
+func SetLogLevel(level int) {
+	loglevel_ = level
+}
+
 func Emergency(i interface{}) {
 	s := fmt.Sprint(i)
 	var _, file, line, _ = runtime.Caller(1)
-	if usegorutine_ {
-		wg_.Add(1)
-		go vlog(0, file, line, s)
-	} else {
-		vlog(0, file, line, s)
-	}
+	go vlog(0, file, line, s)
+}
 
+func EmergencyWait(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	vlog(0, file, line, s)
 }
 
 func Alert(i interface{}) {
 	s := fmt.Sprint(i)
 	var _, file, line, _ = runtime.Caller(1)
-	if usegorutine_ {
-		wg_.Add(1)
-		go vlog(1, file, line, s)
-	} else {
-		vlog(1, file, line, s)
-	}
+	go vlog(1, file, line, s)
+}
 
+func AlertWait(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	vlog(1, file, line, s)
 }
 
 func Critical(i interface{}) {
 	s := fmt.Sprint(i)
 	var _, file, line, _ = runtime.Caller(1)
-	if usegorutine_ {
-		wg_.Add(1)
-		go vlog(2, file, line, s)
-	} else {
-		vlog(2, file, line, s)
-	}
+	go vlog(2, file, line, s)
+}
+
+func CriticalWait(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	vlog(2, file, line, s)
 }
 
 func Error(i interface{}) {
 	s := fmt.Sprint(i)
 	var _, file, line, _ = runtime.Caller(1)
-	if usegorutine_ {
-		wg_.Add(1)
-		go vlog(3, file, line, s)
-	} else {
-		vlog(3, file, line, s)
-	}
+	go vlog(3, file, line, s)
+}
+
+func ErrorWait(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	vlog(3, file, line, s)
 }
 
 func Warning(i interface{}) {
 	s := fmt.Sprint(i)
 	var _, file, line, _ = runtime.Caller(1)
-	if usegorutine_ {
-		wg_.Add(1)
-		go vlog(4, file, line, s)
-	} else {
-		vlog(4, file, line, s)
-	}
+	go vlog(4, file, line, s)
+}
+
+func WarningWait(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	vlog(4, file, line, s)
 }
 
 func Note(i interface{}) {
 	s := fmt.Sprint(i)
 	var _, file, line, _ = runtime.Caller(1)
-	if usegorutine_ {
-		wg_.Add(1)
-		go vlog(5, file, line, s)
-	} else {
-		vlog(5, file, line, s)
-	}
+	go vlog(5, file, line, s)
+}
+
+func NoteWait(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	vlog(5, file, line, s)
 }
 
 func Info(i interface{}) {
 	s := fmt.Sprint(i)
 	var _, file, line, _ = runtime.Caller(1)
-	if usegorutine_ {
-		wg_.Add(1)
-		go vlog(6, file, line, s)
-	} else {
-		vlog(6, file, line, s)
-	}
+	go vlog(6, file, line, s)
+}
+
+func InfoWait(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	vlog(6, file, line, s)
 }
 
 func Debug(i interface{}) {
 	s := fmt.Sprint(i)
 	var _, file, line, _ = runtime.Caller(1)
-	if usegorutine_ {
-		wg_.Add(1)
-		go vlog(7, file, line, s)
-	} else {
-		vlog(7, file, line, s)
-	}
+	go vlog(7, file, line, s)
+}
+
+func DebugWait(i interface{}) {
+	s := fmt.Sprint(i)
+	var _, file, line, _ = runtime.Caller(1)
+	vlog(7, file, line, s)
 }
 
 func Check(err error) bool {
 	if err != nil {
 		s := fmt.Sprintf("check %v", err)
 		var _, file, line, _ = runtime.Caller(1)
-		if usegorutine_ {
-			wg_.Add(1)
-			go vlog(3, file, line, s)
-		} else {
-			vlog(3, file, line, s)
-		}
+		vlog(3, file, line, s)
 		return false
 	}
 	return true
 }
 
 func vlog(eventtype int, file string, line int, str string) {
-	var pid = syscall.Getpid()
 	var sf = strings.Split(file, "/")
 	file = sf[len(sf)-1]
 	t := time.Now().UTC()
 	strt := t.Format("Jan 2 15:04:05 2006")
 
-	var fmtstr = fmt.Sprintf("<%d>%.15s %s[%d]: %s:%d %s: %s", eventtype, strt, name_, pid, file, line, errtypes[eventtype], str)
+	var fmtstr = fmt.Sprintf("<%d>%.15s %s[%d]: %s:%d %s: %s", eventtype, strt, name_, pid_, file, line, errtypes[eventtype], str)
 	fmt.Fprintf(conn_, fmtstr)
 	if consoleout_ {
 		fmt.Fprintf(os.Stderr, "%s\n", fmtstr)
@@ -220,8 +218,5 @@ func vlog(eventtype int, file string, line int, str string) {
 		case 7:
 			syslog_.Debug(fmtstr)
 		}
-	}
-	if usegorutine_ {
-		wg_.Done()
 	}
 }
